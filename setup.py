cf623c759f22953b10a60333e82cb1dcdfbd80d1
doug syer <dsyer@nwnit.com>
2013-11-13 00:11:43 -0500

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.CiscoEnvMonE"
-VERSION = "2.0.2"
+VERSION = "2.0.3"
 AUTHOR = "Doug Syer"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
