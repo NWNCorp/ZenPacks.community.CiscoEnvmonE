ff761c41b6975c76e520d9cf894e74b71f9deacb
doug syer <dsyer@nwnit.com>
2018-09-19 09:50:53 -0400

    update patch

--- ZenPacks/community/CiscoEnvMonE/patches/CiscoMonitorPatch.py
+++ ZenPacks/community/CiscoEnvMonE/patches/CiscoMonitorPatch.py
@@ -1,38 +1,19 @@
 """
 patches for Cisco Monitor.
-The Cisco Monitor zenpack tries to exclude relations inherited from Products.ZenModel.Device and
-OperatingSystem/DeviceHW.  it does this by explicitly setting only some relations to inherit in CiscoDevice.CiscoDevice
-and then calling Device.__init__ and ManagedEnttity.__init__ in a super command.
+The Cisco Monitor zenpack (probably inadvertantly) messes up inherited relations from Device and other os/hw things.
 
-We dont want to completely remove this protection and we dont want to manually patch the file, so we came up with
-this work around which forces the module to load with the patches settings
+even relatinos directly appliked to the ciscoDevice class have issues on instanitation of devices not getting relations.
+but subclasses of CiscoDevice are fine at least they are for me.
 
-You wouldnt need this if you could guarantee that this zenpack would be loaded earlier in the easy_install.pth
-and you dont need to do this for sub-classes of cisco device.
+i have a couple theories on why this is but basically the way they do the relations in CiscoDevice makes those
+relations more static than other classes, in part to protect relations overlap but I think its probably a mistake
+that they have yet to fix and i havent felt like chasing them about it.  even the default calcperf relation
+doesnt take on the Cisco Device (at all even on buildRelations AFIK...)
 
-Even without this patch if you do a buildRelations on the device after it picks up its python class it would get the
-new relation.
-
-There may be a more elegant way of doing this, i know there are more fragile ways and more complicated ways but this
-seems fairly innocent and it is working for other zenpacks and should be fairly future proof.
-
-In this zenpack since we are only updating hwrelations this is pretty safe and shouldnt override the protections on
-the zenpack for device level relations although it could in theory cause an issue with globally overriden base
-hw classes or inherited hw classes applied globally but I think this is a fairly safe thing.
-
-we could also jsut have this a device level relation but i really like having my clear hw components under device.hw and
-os under device .os.
-
-if you had device level relations for cisco device you can follow a similar pattern or you could
-
-Again use caution if you are using this as a template for overriding classes...in production if you break or
-worse change existing relations you can pretty much destroy your zodb sometimes that is temporary ie you just break
-relations
-but if you instantiate then change relations you can be in big trouble since all the wrappers around the zodb/aquisition/
-property code in zenoss makes it really hard to hack things out of the zodb using std zodb t-shooting..
-
-I warned you if you copy this ....be careful know what you are doing and test especially if you are migrationg to differnt
-relationship names with existing objects that have those relations instantiated...
+again i know why and there are ways to add relations to Cisco Device and have it inherit from Device.Device..
+I have dont it n other ZenPacks and it doesnt involve hacking the CiscoDevice.py file...but anyways since we are
+only overriding hwdevice relations we can just make sure that we load up CiscoDevice here fist and this
+SHould fix the issue with Cisco devices not getting this relations without doing a buildRelations..
 
 """
 # pylint: disable=F0002, W0603, W0621, E0602
