c682efe6f6a601913531e2299ec11505a26ae864
doug syer <dsyer@nwnit.com>
2018-10-18 12:02:40 -0400

    version 2.5 export for z6 install

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.CiscoEnvMonE"
-VERSION = "2.1.1"
+VERSION = "2.5.0"
 AUTHOR = "Doug Syer"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
